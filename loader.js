e331194577b0bcb3fe9ac1009a16ad04cb7569af
udueirgvj <lllsgl8j@gmail.com>
2026-02-15 08:16:27 +0300

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,20 @@
+// loader.js - تحميل جميع ملفات JavaScript بالترتيب
+const files = [
+    'js/firebase-config.js',
+    'js/auth.js',
+    'js/bots.js',
+    'js/support.js',
+    'js/groups.js',
+    'js/chat.js',
+    'js/app.js'
+];
+
+function loadScript(index) {
+    if (index >= files.length) return;
+    const script = document.createElement('script');
+    script.src = files[index];
+    script.onload = () => loadScript(index + 1);
+    script.onerror = () => console.error('خطأ في تحميل:', files[index]);
+    document.head.appendChild(script);
+}
+loadScript(0);
